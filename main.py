f19bf85ae62b74dbe8b0e67315e05867caec0f1f
Vlados8593 <v_mulyarchik@bk.ru>
2022-07-01 21:43:01 +0300

    3ая версия. Итоговая(+функции)

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ print('Контрольная работа GB: написать программ
       '\nновый массив из строк, длина которых больше или равна 3')
 input("Нажмите 'Enter' для продолжения")
 print("Вводите слова/цифры/числа через пробел ")
-list = [ input() for i in range(5)]
+list = [input() for i in range(5)]
 print("Текущий массив: ")
 print(list)
 index = 0
-for i in list:
-    if len(i) <= 3:
-        index += 1
 
+
+def array_sorting(array):
+    global index
+    for i in array:
+        if len(i) <= 3:
+            index += 1
+
+
+array_sorting(list)
 second_array = [0 for i in range(index)]
-index = 0
-for i in list:
-    if len(i) <= 3:
-        second_array[index] = i
-        index += 1
-print(second_array)
+
+
+def create_new_array(array):
+    index = 0
+    for i in list:
+        if len(i) <= 3:
+            array[index] = i
+            index += 1
+    print(array)
+
+print('\nПереформированный массив: ')
+create_new_array(second_array)
